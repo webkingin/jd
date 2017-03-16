1a36c72603a1595978b96ce7e0575e258637c667
nnonsense06 <nnonsense06@gmail.com>
2017-03-15 19:54:50 -0700

    fc

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,15 @@
-var gulp = require('gulp')
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+var rename = require("gulp-rename");
 
-gulp.task('generateProdIndexHtml', function() {
-
-});
-
-gulp.task('default', ['generateProdIndexHtml'], function() {
+gulp.task('minify', function() {
+	return gulp.src('index.dev.html')
+	.pipe(htmlmin({
+		collapseWhitespace: true,
+		removeComments: true,
+		removeAttributeQuotes: true
+	}))
+	.pipe(rename("index.html"))
+	.pipe(gulp.dest("."));
 
 });
